03d8faa6b1cff8f16c72c24790f08950b2046871
[D[Drvpandey <rishavpandeyjec@gmail.com>
2019-08-09 11:59:06 +0530

    Downloaded basic skeleton form SpringInizr

---
+++ src/main/java/com/minor/rishav/SpringDatajpa/SpringDatajpaApplication.java
@@ -0,0 +1,13 @@
+package com.minor.rishav.SpringDatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDatajpaApplication.class, args);
+	}
+
+}
